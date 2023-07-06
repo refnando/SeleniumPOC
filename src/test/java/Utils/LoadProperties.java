3fb74c37e00e7338f7428930ab0ce3be91df30ea
refnando <hfer.cc@gmail.com>
2023-07-06 14:43:40 -0600

    🔥Update LoadProperties.java. POM, and user.json has been created

--- src/test/java/Utils/LoadProperties.java
+++ src/test/java/Utils/LoadProperties.java
@@ -1,6 +1,9 @@
 package Utils;
 
+import com.google.gson.Gson;
+
 import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.Properties;
 
@@ -10,9 +13,10 @@ public class LoadProperties {
 
     public LoadProperties(){
         try{
-            FileInputStream fl = new FileInputStream("src/test/java/Utils/Data/user.properties");
-            prop = new Properties();
-            prop.load(fl);
+            Gson gson = new Gson();
+            FileReader fl = new FileReader("src/test/java/Utils/Data/user.json");
+            prop = gson.fromJson(fl, Properties.class);
+            fl.close();
         }catch (IOException e){
             e.printStackTrace();
         }
